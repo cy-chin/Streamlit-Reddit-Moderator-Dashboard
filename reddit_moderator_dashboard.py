9e73f5b49c26aa6f9ebc02c7f9149b4ba83180bf
cy-chin <ryanchin.mail@gmail.com>
2024-05-21 11:47:38 +0800

    Update reddit_moderator_dashboard.py

--- reddit_moderator_dashboard.py
+++ reddit_moderator_dashboard.py
@@ -172,7 +172,7 @@ button1 = st.button('Start Analyzing')
 
 if button1:
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    response = requests.get(url, headers={'User-agent': 'MyStreamlitApp/1.0'})
+    response = requests.get(url, headers={'User-agent': 'StreamlitApp/1.0'})
     if response.status_code == 200:
         st.session_state.prediction_outcome = 0 #reset prediction outcome
         my_bar = st.progress(0.1, text=progress_text)
